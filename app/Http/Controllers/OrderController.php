27c16f9a9474d66cb0352a1534dcb527d410d5bb
Carl <wazzy42069@gmail.com>
2025-01-05 09:33:23 +0800

    Commit

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Notifications\OrderStatusUpdated;
 use App\Models\Order;
 use App\Models\OrderItem;
 use App\Models\Product;
--- resources/views/emails/order_confirmation.blade.php
+++ resources/views/emails/order_confirmation.blade.php
@@ -7,12 +7,12 @@
     <h1>Order Confirmation</h1>
     <p>Thank you for your order!</p>
     <p>Your order ID is: {{ $order->id }}</p>
-    <p>Total amount: ${{ number_format($order->total_amount / 100, 2) }}</p>
+    <p>Total amount: ${{ number_format($order->total_amount, 2) }}</p>
     
     <h3>Order Details:</h3>
     <ul>
         @foreach($order->orderItems as $item)
-            <li>{{ $item->product_name }} (x{{ $item->quantity }}) - ${{ number_format($item->price * $item->quantity / 100, 2) }}</li>
+            <li>{{ $item->product_name }} (x{{ $item->quantity }}) - ${{ number_format($item->price * $item->quantity, 2) }}</li>
         @endforeach
     </ul>
     
